dbde8d59e96a652ec135daae1617c123c63a42cc
Chris Blackburn <chris@midwiretech.com>
2016-03-15 12:20:04 -0500

    Remove initial start_page visit

--- lib/tr4n5l4te/translator.rb
+++ lib/tr4n5l4te/translator.rb
@@ -2,23 +2,22 @@ require 'open-uri'
 
 module Tr4n5l4te
   class Translator
-    START_PAGE = 'https://translate.google.com/'.freeze
+    START_PAGE = 'https://translate.google.com'.freeze
 
     attr_reader :sleep_time, :agent
 
     def initialize(args = {})
       @sleep_time = args.fetch(:sleep_time, 2)
       @agent = Agent.new
-      load_cookies
-      agent.visit(START_PAGE)
-      sleep_default
-      store_cookies
     end
 
     def translate(text, from_lang, to_lang)
+      return '' if text.trim.empty?
       encoded_text = URI.encode(text)
-      url = "https://translate.google.com/##{from_lang}/#{to_lang}/#{encoded_text}"
+      url = "#{START_PAGE}/##{from_lang}/#{to_lang}/#{encoded_text}"
+      load_cookies
       agent.visit(url)
+      store_cookies
       sleep_default
       result_box = browser.find('#result_box')
       result_box.text
--- spec/lib/tr4n5l4te/translator_spec.rb
+++ spec/lib/tr4n5l4te/translator_spec.rb
@@ -5,12 +5,6 @@ module Tr4n5l4te
     let(:translator) { described_class.new }
 
     if ENV.fetch('INTEGRATION', false)
-      context '#new' do
-        it 'returns the proper thing' do
-          expect(translator).to be_a(described_class)
-        end
-      end
-
       context '.translate' do
         it 'translates a string' do
           expect(translator.translate('hello', :en, :es)).to match(/hola/i)
@@ -19,6 +13,19 @@ module Tr4n5l4te
         it 'translates another string' do
           expect(translator.translate('how are you', :en, :es)).to match(/cómo estás/i)
         end
+      end
+    end
+
+    context '#new' do
+      it 'returns the proper thing' do
+        expect(translator).to be_a(described_class)
+      end
+    end
+
+    context '.translate' do
+      it 'returns an empty string if the argument is empty' do
+        expect(translator).to_not receive(:load_cookies)
+        expect(translator.translate('', :en, :es)).to eq('')
       end
     end
   end
